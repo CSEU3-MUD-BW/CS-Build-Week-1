02815d009307b102e9b97bef4d2b97be2f9591ba
CarnunMP <carnun@hotmail.co.uk>
2020-03-02 20:12:20 +0000

    Made a couple more tweaks to description generation.

--- util/room_content_generator.py
+++ util/room_content_generator.py
@@ -18,7 +18,7 @@ rules = {
         f'[roomName:engine-room][feature:{basicRoomFeatures},pipe,reactor,green gelatinous puddle]',
     ],
     'roomAdjective': ['dark', 'smoky', 'dangerous-looking', 'messy', 'quiet', 'irradiated', 'full', 'empty', 'packed', 'stripped', 'ransacked'],
-    'featureAdjective': ['smoking', 'broken', 'flashing', 'beeping', 'oversized', 'odd-looking', 'unfamiliar', 'sharp', 'hot'],
+    'featureAdjective': ['smoking', 'broken', 'flashing', 'beeping', 'oversized', 'odd-looking', 'unfamiliar', 'sharp', 'hot', 'regular'],
 
     'adverb': ['suspiciously', 'strangely', 'worryingly', 'puzzlingly', 'suprisingly', 'curiously'],
     'setTitleType': ['#roomAdjective.capitalize#', '#adverb.capitalize# #roomAdjective.capitalize#'],
@@ -27,9 +27,12 @@ rules = {
     'setFeatureType': ['#feature#', '#featureAdjective# #feature#'],
     'description': [
         'You see ~#setFeatureType.a#~.', #Note: ~tildes~, here, make it possible to scrape features (items) from the room string.
+        'You see ~#setFeatureType.a#~. And right next to it, ~#setFeatureType.a#~!',
         'You bend over, and spot several ~#setFeatureType.s#~.',
         'You hear a sound behind you and turn around. At your feet is ~#setFeatureType.a#~.',
-        'You smell something strange. Following your nose, you find ~#setFeatureType.a#~ and some ~#setFeatureType.s#~. Odd!'
+        'You smell something strange. Following your nose, you find ~#setFeatureType.a#~ and some ~#setFeatureType.s#~.',
+        'You peek round the door. There\'s nothing in the room but ~#setFeatureType.a#~.',
+        'On the wall you see ~#setFeatureType.a#~. And on the ceiling, ~#setFeatureType.a#~.'
     ],
 
     'origin': '#[#setRoomName#]title#: #description#'
@@ -66,15 +69,18 @@ def removeTildes(description):
 
     return output
 
-rooms = []
-for i in range(50):
-    roomString = grammar.flatten('#origin#')
-    splitRoomString = roomString.split(": ")
+def generate_room_content(n=100):
+    rooms = []
+    for i in range(n):
+        roomString = grammar.flatten('#origin#')
+        splitRoomString = roomString.split(": ")
 
-    rooms.append({
-        'title': splitRoomString[0],
-        'description': removeTildes(splitRoomString[1]),
-        'items': scrapeItems(splitRoomString[1])
-    })
+        rooms.append({
+            'title': splitRoomString[0],
+            'description': removeTildes(splitRoomString[1]),
+            'items': scrapeItems(splitRoomString[1])
+        })
 
-print(rooms)
+    return rooms
+
+print(generate_room_content(1))
